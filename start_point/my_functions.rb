94127c9a6a3dfadc5764c3e3c55888aef164550d
Alison Williams <alison.lauriewill@gmail.com>
2019-04-03 15:30:34 +0100

    completed first 2 tests

---
+++ start_point/my_functions.rb
@@ -0,0 +1,16 @@
+def add_array_lengths(array1, array2)
+  result = array1.length + array2.length
+  return result
+end
+
+def sum_array(numbers)
+  result = 0
+  for number in numbers
+    result += number
+  end
+  return result
+end
+
+def find_item
+  
+end
